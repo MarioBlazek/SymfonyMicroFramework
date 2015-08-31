a2ffa7cedb6279789ae0191daed9cf69e053be6b
Marek <blazek.mario@gmail.com>
2015-08-31 08:42:23 +0200

    Part 4

--- AppKernel.php
+++ AppKernel.php
@@ -23,6 +23,8 @@ class AppKernel extends Kernel
             new \AppBundle\AppBundle(),
         );
 
+        $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+
         return $bundles;
     }
 
